d8762cfae5d38e3b297d1ba4d2e2a37337d02d41
Gordon DuQuqesnay <gduquesnay@gmail.com>
2014-05-20 16:10:39 +0900

    point to my dotfiles not samnangs

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
-git clone git://github.com/samnang/dotfiles.git ~/.dotfiles
+git clone git://github.com/gduquesnay/dotfiles.git ~/.dotfiles
 
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
